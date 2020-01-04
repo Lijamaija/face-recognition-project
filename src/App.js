02e6b85bfddb1e4efbd0d1e734842ed4edfe17fd
Lijamaija <liza.timofeeva@gmail.com>
2020-01-04 21:35:55 +0100

    working version without sign-in form

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ class App extends Component {
             leftCol: clarifaiFace.left_col * width,
             topRow: clarifaiFace.top_row * height,
             rightCol: width - (clarifaiFace.right_col * width),
-            bottomRow: height - (clarifaiFace.bottomRow * height)
+            bottomRow: height - (clarifaiFace.bottom_row * height)
 
         }
     }
