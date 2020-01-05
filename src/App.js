3f87dcf107fadeaa47ba46287815fe3f7d003c9a
Lijamaija <liza.timofeeva@gmail.com>
2020-01-05 22:09:59 +0100

    registration form nearly finished

--- src/App.js
+++ src/App.js
@@ -94,18 +94,21 @@ class App extends Component {
               <Particles className='particles'
                 params={particlesOptions} />
             <Navigation onRouteChange={this.onRouteChange} />
-            {this.state.route === 'signin' ?
-                <Signin onRouteChange={this.onRouteChange}/>
-              :<div>
+            { this.state.route === 'home'
+              ? <div>
                     <Logo />
                     <Rank />
-                    <Register/>
                     <ImageLinkForm
                       onInputChange={this.onInputChange}
-                        onButtonSubmit={this.onButtonSubmit}
+                      onButtonSubmit={this.onButtonSubmit}
                      />
-                 <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} /> 
-               </div>
+                    <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} /> 
+                </div>
+                : (
+                    this.state.route === 'signin'
+                        ? <Signin onRouteChange={this.onRouteChange} />
+                        : <Register onRouteChange={this.onRouteChange} />
+                )
             }
         </div>
       );
